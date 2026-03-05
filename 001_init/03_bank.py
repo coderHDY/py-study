8505c8900979bcf183a23b1883b82d2b02d62033
coder-HDY <d_huang@dreamarts.co.jp>
2026-03-05 15:42:28 +0800

    feat: skills

--- 001_init/03_bank.py
+++ 001_init/03_bank.py
@@ -1,17 +1,26 @@
-have_money = 10000
+balance = 10000
 account = "coderHDY"
 password = "pas123"
 
 input_account = input("请输入账号：")
-
-if (input_account != account):
+if input_account != account:
     print("无效账户")
     exit()
-else:
-    input_password = input("请输入密码：")
-    if input_password != password:
-        print("密码错误")
-        exit()
-    else: 
-        input_get_money = int(input("要取的钱："))
-        print(f"您取走了{input_get_money}元，您的账户还剩{have_money - input_get_money}元！")
+
+input_password = input("请输入密码：")
+if input_password != password:
+    print("密码错误")
+    exit()
+
+try:
+    amount = int(input("要取的钱："))
+except ValueError:
+    print("请输入有效金额")
+    exit()
+
+if amount > balance:
+    print(f"余额不足，当前余额为{balance}元")
+    exit()
+
+balance -= amount
+print(f"您取走了{amount}元，您的账户还剩{balance}元！")
